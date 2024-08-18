d44e029228a82c3afe5c970d804594e5be1db623
pastre <bruno.pastre@onefootball.com>
2024-08-18 15:26:21 +0200

    feat: prints test result

--- stest.sh
+++ stest.sh
@@ -1,5 +1,5 @@
 TEST_RUN_FOLDER=${TEST_RUN_FOLDER:-"$TMPDIR"}
-
+TEST_FAILED=""
 verify() {
 	TEST_CASE_NAME=$1
 	EXPECTATION=$2
@@ -7,14 +7,13 @@ verify() {
 	TEST_FOLDER="$TEST_RUN_FOLDER/$(md5 <<< $TEST_CASE_NAME)"
 	mkdir -p $TEST_FOLDER
 	echo "Running $TEST_CASE_NAME" > "$TEST_FOLDER/status"
-	bash -c "$TEST_CASE" > "$TEST_FOLDER/result"
+	bash -c "$TEST_CASE" 2>&1 > "$TEST_FOLDER/result"
 	if test 0 -eq $? && test "succeeds" = $EXPECTATION; then
 		echo "$TEST_CASE_NAME: Passed" > "$TEST_FOLDER/status"
-	elif test 0 -ne $? && test "fails" = $EXPECTATION; then
+	elif test 1 -eq $? && test "fails" = $EXPECTATION; then
 		echo "$TEST_CASE_NAME: Passed" > "$TEST_FOLDER/status"
 	else
 		echo "$TEST_CASE_NAME: Failed" > "$TEST_FOLDER/status"
-		cat  "$TEST_FOLDER/result"
 	fi
 }
 
@@ -35,6 +34,14 @@ find . -name '*_test.sh' | while read file; do
 	bash $file
 done
 
-find $TEST_RUN_FOLDER -name 'status' | while read file; do
-	cat "$file"
-done
+TEST_FAILED=$(grep -Ril "Failed" $TEST_RUN_FOLDER)
+
+if [ -n "$TEST_FAILED" ]; then
+	echo "Tests failed"
+	grep -Ril "Failed" $TEST_RUN_FOLDER | while read file; do
+		cat $file
+	done
+	exit -1
+else
+	echo "All tests passed"
+fi
